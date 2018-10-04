f6690556d02493434e40342baf23cde49e22b718
Dina <dmrabanal@gmail.com>
2018-10-03 20:33:17 -0600

    Initial commit

---
+++ src/Euler1.java
@@ -0,0 +1,18 @@
+public class Euler1 {
+
+  public static void main(String[] args) {
+    System.out.println(Sum(1000));
+  }
+
+  public static int Sum(int total) {
+    int sum = 0;
+    for (int i = 0; i < 1000; i++) {
+      if (i % 5 == 0 || i % 3 == 0)
+        ;
+      sum += i;
+
+    }
+
+      return sum;
+    }
+}
